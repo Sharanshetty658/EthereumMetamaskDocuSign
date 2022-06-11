ebe407458253ad98faf6c10e96b61011a97bf119
yosnai chanatrutipan <GunUltimateID@gmail.com>
2022-06-11 19:14:51 +0700

    moved pc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { APIClient, Openlaw } from "openlaw";
+
 import ContractForm from './components/Form';
 
 
@@ -8,12 +10,30 @@ import ContractForm from './components/Form';
 function App() {
 
   // form information
-  const [formState,setFormState] = useState();
+  const [template,setTemplate] = useState(`**NAME**[[firstname]]`);
+  const [parameters,setParameters] = useState({});
+  const editableTemplateRef = useRef();
+
+  function onParametersChange(key,value,validationData){
+    setParameters({ ...parameters, [key]: value });
+  }
+  function onTemplateChange(e){
+    const { compiledTemplate } = Openlaw.compileTemplate(template);
+    const { executionResult, errorMessage } = Openlaw.execute( compiledTemplate, {},{});
+    console.log("error is", errorMessage);
+    if (!errorMessage){
+      setTemplate(e.target.value)
+      console.log("changed template:", template);}
+  }
 
   return (
+    <>
+    <textarea ref={editableTemplateRef} onChange={onTemplateChange}>{template}</textarea>
+
     <div className="App">
-      <ContractForm stateLift={setFormState}/>
+      <ContractForm template={template} onChange={onParametersChange}/>
     </div>
+    </>
   );
 }
 
