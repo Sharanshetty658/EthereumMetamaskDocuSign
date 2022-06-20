0c313cf5b256dc58c6f13b9abb28bde61a040448
c.yosnai <6372096621@student.chula.ac.th>
2022-06-21 02:30:35 +0700

    add sendToBlockchain functionality

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ import ContractForm from "./components/Form";
 import { ethers } from "ethers";
 import { address, abi } from "./utility/smartcontract";
 import { sha256 } from "crypto-hash";
-
+import SendIcon from "@mui/icons-material/Send";
+import CloseIcon from "@mui/icons-material/Close";
 import {
   Grid,
   TextField,
   Box,
+  Paper,
+  Alert,
   Typography,
   CircularProgress,
   Backdrop,
+  Button,
 } from "@mui/material";
 import Preview from "./components/Preview";
 import { convertTemplateToHTML } from "./utility/toHTML";
 import { isValidTemplate } from "./utility/isValid";
 import { Container } from "@mui/system";
 import ToBlockchain from "./components/ToBlockchain";
+import ropsten from "./utility/ropsten.png";
 const apiClient = new APIClient("https://lib.openlaw.io/api/v1/default");
 
 const style = {
@@ -39,11 +44,8 @@ function App() {
   const [signed_msg, setsignedMsg] = useState("");
   const [file_input, setFileInput] = useState("");
   const [hashed, setHashed] = useState();
-
-  const formUpdate = (key, value, validationData) => {
-    setFormdata({ ...formData, [key]: value });
-    //console.log(formData)
-  };
+  const [detail, setDetail] = useState();
+  const [alert, setAlert] = useState(false);
 
   useEffect(() => {
     const connectToMetamask = async () => {
@@ -60,7 +62,13 @@ function App() {
     connectToMetamask();
   }, [eth_address]);
 
+  const formUpdate = (key, value, validationData) => {
+    setFormdata({ ...formData, [key]: value });
+    //console.log(formData)
+  };
+
   const handleFileInput = (e) => {
+    e.preventDefault();
     const pdf = new FileReader();
     var hashed;
     pdf.onload = async () => {
@@ -71,39 +79,31 @@ function App() {
     };
 
     pdf.readAsArrayBuffer(e.target.files[0]);
+    setPopup(true);
   };
 
-  async function sendToBlockchain() {
+  async function SendToBlockchain() {
     if (window.ethereum) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-      //const accounts = await provider.send("eth_requestAccounts", []);
-      //console.log(accounts)
-      //setEthaddress(accounts[0]);
-      //console.log("address", eth_address);
       const signer = provider.getSigner();
       const openlawThai = new ethers.Contract(address, abi, provider);
       const openlawThaiSigner = openlawThai.connect(signer);
       let ans = await openlawThai.wave();
       var now = new Date();
-
-      let storing = await openlawThaiSigner.store(now.toString(), 1234, "wtf");
+      //let storing = await openlawThaiSigner.store(now.toString(), 1234, "wtf");
       setMsg(ans);
-      setsignedMsg(storing);
-      //console.log("WTF");
-      console.log("msg ", msg);
-      console.log("signed msg ", msg);
+      //setsignedMsg(storing);
+      setAlert(true);
     } else {
       alert("install metamask extension!!");
     }
-    setPopup(true);
   }
 
   function submitTemplate(e) {
     e.preventDefault();
     setKey(key + 1);
     setFormdata({});
-
+    setAlert(false);
     if (isValidTemplate(textfieldRef.current.value))
       setTemplate(textfieldRef.current.value);
     else
@@ -132,6 +132,10 @@ function App() {
         }}
       >
         <Link to="/">Home</Link> | <Link to="/contracts">view contracts</Link> |{" "}
+        <span style={{ float: "right" }}>
+          Smart Contract location (ropsten test network) :
+          "0xcb51e09ba325d43123d2fed346150afbfcf64dbf"
+        </span>
       </nav>
       <Outlet />
 
@@ -172,13 +176,15 @@ function App() {
               Preview
             </Typography>{" "}
             <button onClick={downloadPDF}> Download PDF</button>
-            <button onClick={sendToBlockchain}>
-              {" "}
-              SIGNED with metamask and SEND to blockchain
-            </button>
-            <input type="file" className="form-control" id="file-input" onChange={handleFileInput}/>
             <hr />
             <Preview template={template} formData={formData} />
+            <Button
+              variant="contained"
+              endIcon={<SendIcon />}
+              onClick={() => setPopup(true)}
+            >
+              Send to Blockchain
+            </Button>
           </Box>
         </Grid>
       </Grid>
@@ -186,9 +192,79 @@ function App() {
       <Backdrop
         sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={popup}
-        onClick={() => setPopup(false)}
       >
-        <ToBlockchain SignedBy={eth_address} />
+        <Box
+          sx={{
+            width: 0.8,
+            height: "50vh",
+            textAlign: "center",
+            bgcolor: "white",
+          }}
+        >
+          Upload file
+          <CloseIcon
+            sx={{ float: "right", color: "black" }}
+            onClick={() => {
+              setPopup(false);
+              setAlert(false);
+            }}
+          />
+          <br />
+          <Typography
+            variant="subtitle2"
+            component="h6"
+            textAlign="center"
+            color="#6b6b61"
+          >
+            Upload file : contract's pdf you downloaded above, or any other
+            document that you want to store as a hashed on the blockchain
+          </Typography>
+          <br />
+          <div id="file-input">
+            <input type="file" onChange={handleFileInput} />
+          </div>
+          <br />
+          <Typography
+            variant="subtitle2"
+            component="h6"
+            textAlign="center"
+            color="#6b6b61"
+          >
+            type some short description about what is this contract about
+          </Typography>
+          <br />
+          <TextField
+            sx={{ width: 0.5 }}
+            variant="filled"
+            onChange={(e) => setDetail(e.target.value)}
+          />
+          <br />
+          <br /> <br />
+          <br />
+          <Typography
+            variant="subtitle2"
+            component="h6"
+            textAlign="center"
+            color="#6b6b61"
+          >
+            Before you sign, set your metamask to Ropsten test network and
+            please make sure that you have some Ether on that account
+          </Typography>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={SendToBlockchain}
+          >
+            Sign tracsaction with Metamask and send to Blockchain (cost gas)
+          </Button>
+          <br />
+          {alert && (
+            <Alert severity="success">
+              Contract succesfully deployed on blockchain , check "view deployed
+              contract" to access this contracts on blockchain{" "}
+            </Alert>
+          )}
+        </Box>
       </Backdrop>
     </>
   );
--- src/components/ToBlockchain.js
+++ src/components/ToBlockchain.js
@@ -1,20 +1,15 @@
 import { Box, Paper } from "@mui/material";
 const ToBlockchain = ({ SignedBy }) => {
-  
   //console.log("signedBy", SignedBy);
   return (
     <>
-    <Box sx={{width:0.7,textAlign:"center"}}>
-      <Paper elevation={3}>
-        <hr />
-        Signed by ETH ACCOUNT: {SignedBy}
-        <br /> Contract hash: _____
-        <br />
-        Signed Date_____ <br /> Contract Detail :_______
-        <br /> Contract address: _______
-        <hr />
-      </Paper>
-      </Box>
+      <hr />
+      Signed by ETH ACCOUNT: {SignedBy}
+      <br /> Contract hash: _____
+      <br />
+      Signed Date_____ <br /> Contract Detail :_______
+      <br /> Contract address: _______
+      <hr />
     </>
   );
 };
