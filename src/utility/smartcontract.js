12d84dc6ba5223d5c13bea11620d3151dcd95559
yosnai chanatrutipan <GunUltimateID@gmail.com>
2022-06-27 20:37:52 +0700

    rec

--- src/utility/smartcontract.js
+++ src/utility/smartcontract.js
@@ -1,5 +1,63 @@
-export const address = "0x34c9bdb4d92d4ddf6ab7748aedc2ab66bb93d6af";
-export const abi =[
+export const address = "0x8bd126632b72715929B1FE7DaF1Ea92BD7D8d3B8";
+export const abi = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_created_at",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_signed_at",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_hashed",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_detail",
+				"type": "string"
+			}
+		],
+		"name": "create",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deleteAll",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_signed_at",
+				"type": "string"
+			}
+		],
+		"name": "sign",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"inputs": [],
 		"stateMutability": "nonpayable",
@@ -57,46 +115,6 @@ export const abi =[
 			}
 		],
 		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_recipient",
-				"type": "address"
-			},
-			{
-				"internalType": "string",
-				"name": "_created_at",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_signed_at",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_hashed",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_detail",
-				"type": "string"
-			}
-		],
-		"name": "create",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "deleteAll",
-		"outputs": [],
-		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
@@ -275,24 +293,6 @@ export const abi =[
 			}
 		],
 		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_id",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "_signed_at",
-				"type": "string"
-			}
-		],
-		"name": "sign",
-		"outputs": [],
-		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
